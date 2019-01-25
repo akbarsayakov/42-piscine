1087e18e8d4a2c7ad69c3236f13900f59a0a26d3
akbarsayakov <38359586+akbarsayakov@users.noreply.github.com>
2019-01-24 23:25:36 -0800

    Day05

---
+++ Day05/ex04/ft_strncpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asayakov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/15 16:12:07 by asayakov          #+#    #+#             */
+/*   Updated: 2019/01/15 16:57:59 by asayakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	char			*ptr;
+	unsigned int	i;
+
+	ptr = dest;
+	i = 0;
+	while (i < n && *src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	while (i < n)
+	{
+		*dest = '\0';
+		dest++;
+		i++;
+	}
+	return (ptr);
+}
